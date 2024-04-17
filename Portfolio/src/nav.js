641050696df5d828ca2c034bae4ed95929f638cb
Quinten Scholtes <quintenscholtes@hotmail.com>
2024-04-17 20:58:20 +0200

    Nav and landing page

---
+++ Portfolio/src/nav.js
@@ -0,0 +1,17 @@
+import "./nav.css"
+function NavBar() {
+    return (
+
+        <section >
+    <nav class="fill">
+        <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Resume</a></li>
+        </ul>
+    </nav>
+    </section>
+    )
+}
+
+export default NavBar
