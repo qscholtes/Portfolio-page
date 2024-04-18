4f778d066ec5c7799b57149780fd3be26e8bc3ef
Quinten Scholtes <quintenscholtes@hotmail.com>
2024-04-18 15:48:33 +0200

    Spring library testing

---
+++ Portfolio/src/Components/Skills.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import { useTrail, a } from '@react-spring/web'
+
+import styles from './styles.module.css'
+
+interface TrailProps {
+  open: boolean;
+  children: React.ReactNode; // Add this line to define children prop
+}
+
+const Trail: React.FC<TrailProps> = ({ open, children }) => {
+  const items = React.Children.toArray(children)
+  const trail = useTrail(items.length, {
+    config: { mass: 20, tension: 2000, friction: 1000 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 100,
+    height: open ? 180 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
+  })
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <a.div key={index} className={styles.trailsText} style={style}>
+          <a.div style={{ height }}>{items[index]}</a.div>
+        </a.div>
+      ))}
+    </div>
+  )
+}
+
+export default function Skills() {
+  const [open, set] = useState(true)
+  return (
+    <div className={styles.container} onClick={() => set(state => !state)}>
+      <Trail open={open}>
+        <span>HTML</span>
+        <span>CSS</span>
+        <span>Javascript</span>
+        <span>Responsive Design</span>
+        <span>React.js</span>
+      </Trail>
+    </div>
+  )
+}
--- Portfolio/src/pages/HomePage.js
+++ Portfolio/src/pages/HomePage.js
@@ -1,15 +1,18 @@
 import React from "react";
 import HeaderAnimated from "../Components/HeaderAnimated";
+import Skills from "../Components/Skills.tsx";
 export default function HomePage() {
 return (
   <>
     <HeaderAnimated content="Welcome"></HeaderAnimated>
-    <p> Hello! I'm Quinten, a first-year Computer Science student with a passion for creating immersive web applications.
-     
-
-</p>
-    
-    
+    <p> Hello! I'm Quinten, a first-year Computer Science student with a interest for creating immersive web applications.
+    </p>
+    <p>I was born in the Netherlands and grew up in Germany where I attended an International school. Through growing up in such a 
+      diverse environment I am able to fluently speak English,German and Dutch. </p>
+    <p> The skill-set that I have accumulated so far consists of :  </p>
+    <Skills></Skills>
+    <p>
+    </p>
   </>
 );
 }
