fdbd0ddfe4cee848c81dfc5c1d613341c0fe5166
Quinten Scholtes <quintenscholtes@hotmail.com>
2024-04-18 16:12:02 +0200

    Text landing page

--- Portfolio/src/pages/HomePage.js
+++ Portfolio/src/pages/HomePage.js
@@ -11,7 +11,11 @@ return (
       diverse environment I am able to fluently speak English,German and Dutch. </p>
     <p> The skill-set that I have accumulated so far consists of :  </p>
     <Skills></Skills>
-    <p>
+    <p> Over the course of my university degree I will be introduced to C++, Python and several other frameworks.
+      Currently in my free time I am actively going through the process of improving my understanding and knowledge of frameworks
+      such as React.js, this includes creating websites for fun and as a method of practice.
+      Within the domain of Web development I have only been able to explore the front-end domain so far. I would like to 
+      get introduced delve deeper and solidify my skills before jumping into the world of back-end development. 
     </p>
   </>
 );
